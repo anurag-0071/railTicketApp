b7d1720144655a48eeeb1e976b579ec5b78959a7
Anurag <anurag@storeking.in>
2020-05-24 22:59:27 +0530

    Add new files

---
+++ api/models/ticketModel.js
@@ -0,0 +1,76 @@
+
+const Mongoose = require("mongopse");
+
+const ticketSchema = require("../helpers/schemas/ticket");
+
+const ticketModel = Mongoose.model("Ticket", ticketSchema, "traintickets");
+
+const create = (station) => {
+    return new Promise((resolve, reject) => {
+        ticketModel.create(station).exec().then(newTicket => {
+            console.log("Successfully created ticket", newTicket);
+            resolve(newTicket);
+        }).catch(err => {
+            console.error("Error in ticket creation", err);
+            reject({
+                message: "Technical Error.",
+                error: err.toString()
+            })
+        })
+    })
+}
+
+const findOne = (filter = {}, select = {}) => {
+    return new Promise((resolve, reject) => {
+        ticketModel.findOne(filter).select(select).exec().then(ticketDoc => {
+            resolve(ticketDoc);
+        }).catch(err => {
+            console.error("Error in fetching user document", err);
+            reject({
+                message: "Techincal Error",
+                error: err.toString()
+            });
+        });
+    })
+};
+
+const find = (
+    filter = {},
+    select = {},
+    page = 0,
+    count = 10,
+    sort = { createdAt: -1 }
+) => {
+    return new Promise((resolve, reject) => {
+        ticketModel.find(filter).skip(page * count).count(count).sort(sort).select(select).exec().then(tickets => {
+            resolve(tickets);
+        }).catch(err => {
+            reject({
+                message: "Technical Error",
+                error: err.toString()
+            })
+        })
+    })
+}
+
+
+const update = (filter, updates) => {
+    return new Promise((resolve, reject) => {
+        ticketModel.update(filter, {
+            $set: updates
+        }).exec().then(result => {
+            console.log("update successfull");
+            resolve();
+        }).catch(err => {
+            console.log("error in update action", err);
+            reject(err);
+        })
+    })
+}
+
+module.exports = {
+    create,
+    findOne,
+    update,
+    find,
+}
