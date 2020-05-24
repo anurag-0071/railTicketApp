80aa9ac87e603f5686e5d08c8e40df4626d85279
anurag-0071 <47929115+anurag-0071@users.noreply.github.com>
2020-05-24 13:57:02 +0530

    Feature/users (#3)
    * Implement user controllers
    
    * Add user files
    
    Co-authored-by: Anurag <anurag@storeking.in>

---
+++ api/helpers/schemas/user.js
@@ -0,0 +1,93 @@
+
+const Mongoose = require("mongoose");
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+
+const userDefinition = {
+    _id: Mongoose.Schema.Types.ObjectId,
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        minLength: 6
+    },
+    role: {
+        type: String,
+        enum: ["Admin", "Customer"]
+    },
+    userType: {
+        type: String,
+        enum: ["Admin", "Customer"]
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    isNewUser: {
+        type: Boolean,
+        default: true
+    },
+    salt: String,
+    password: String,
+    lastUpdated: Date,
+    createdAt: {
+        type: Date,
+        default: new Date()
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+}
+
+const userSchema = new Mongoose.Schema(userDefinition);
+
+userSchema.methods.validatePassword = function validatePassword(password) {
+    const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, "sha512").toString("hex");
+    return this.password === hash;
+}
+
+userSchema.method.generateJWTToken = function generateJWTToken() {
+    const userInfo = Object.assign({}, {
+        username: this.username,
+        role: this.role,
+        userType: this.userType,
+    });
+    const token = jwt.sign(userInfo, "railTicketSecret", { expiresIn: "1 days" });
+    return token;
+}
+
+
+userSchema.pre("save", function (next) {
+    this.lastUpdated = new Date;
+    next();
+});
+userSchema.pre("save", function encryptPassword(next) {
+    if (this.isNewUser || this.updatePassword) {
+        this.salt = crypto.randomBytes(16).toString("hex");
+        this.password = crypto.pbkdf2Sync(this.password, this.salt, 1000, 64, "sha512").toString("hex");
+        this.isNewUser = false;
+        next();
+    } else {
+        next();
+    }
+})
+
+
+userSchema.pre("update", function (next) {
+    this.lastUpdated = new Date();
+    next();
+});
+
+
+module.exports = {
+    userSchema
+}
---
+++ api/models/userModel.js
@@ -0,0 +1,63 @@
+
+const Mongoose = require("mongoose");
+
+const userSchema = require("../helpers/schemas/user");
+
+const UserModel = Mongoose.model("User", userSchema, "users");
+
+const createUser = (user) => {
+    return new Promise((resolve, reject) => {
+        UserModel.create(user).exec().then(newUser => {
+            console.log("Successfully created user", newUser);
+            resolve(newUser);
+        }).catch(err => {
+            console.error("Error in user creation", err);
+            reject({
+                message: "Technical Error.",
+                error: err.toString()
+            })
+        })
+    })
+}
+
+const findOne = (filter = {}, select = {}) => {
+    if (!filter.hasOwnProperty("isDeleted")) {
+        filter.isDeleted = false;
+    }
+    return new Promise((resolve, reject) => {
+        UserModel.findOne(filter).select(select).exec().then(userDoc => {
+            resolve(userDoc);
+        }).catch(err => {
+            console.error("Error in fetching user document", err);
+            reject({
+                message: "Techincal Error",
+                error: err.toString()
+            });
+        });
+    })
+}
+
+const find = (
+    filter = {},
+    select = {},
+    page = 0,
+    count = 10,
+    sort = { createdAt: -1 }
+) => {
+    return new Promise((resolve, reject) => {
+        UserModel.find(filter).skip(page * count).count(count).sort(sort).select(select).exec().then(users => {
+            resolve(users);
+        }).catch(err => {
+            reject({
+                message: "Technical Error",
+                error: error.toString()
+            })
+        })
+    })
+}
+
+module.exports = {
+    createUser,
+    find,
+    findOne,
+}
